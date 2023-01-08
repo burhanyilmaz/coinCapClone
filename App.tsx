188ab5ba4e927451fb3a3bd1896d8be9070d28f3
Burhan YILMAZ <burhan.yilmaaz@gmail.com>
2023-01-08 15:38:36 +0300

    feat: prepared codebase

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
+import CustomButton from 'components/core/Button';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <CustomButton title='Coin Cap Clone' onPress={() => {}} />
     </View>
   );
 }
